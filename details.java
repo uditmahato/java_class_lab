797b360723e1f883341366deff359f8e6c23b503
uditmahato <uditmahato29271@gmail.com>
2021-03-21 10:00:53 +0545

    First commit

---
+++ details.java
@@ -0,0 +1,5 @@
+public class details {
+    public static void main (String[] args){
+        System.out.println("Name:Udit Kumar Mahato\t\nRoll:Bcs 2020\t\nclass:Ug\t\nIntake:Sept 2020");
+    }
+}
