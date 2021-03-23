d7483eef59380cc8f795f7329968ddb752c95b92
uditmahato <uditmahato29271@gmail.com>
2021-03-23 07:42:28 +0545

    added program of second class

---
+++ InputUser.java
@@ -0,0 +1,13 @@
+//w.a.p to take input from user and print the sum on the creen
+import java.util.Scanner;
+public class InputUser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("sum of a+b ="+ sum );
+    }
+}
